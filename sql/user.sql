b14ac1a8245d63c6788360e1960f4c174bd68edf
Semir Mahovkic <semir.mahovkic@ministryofprogramming.com>
2018-10-01 23:42:00 +0200

    get user by email pg function

--- sql/user.sql
+++ sql/user.sql
@@ -4,7 +4,7 @@ CREATE TABLE users (
     username varchar(50) NOT NULL,
     email varchar(255) NOT NULL,
     password varchar(255),
-    created_at timestamp DEFAULT NOW(),
+    created_at integer DEFAULT EXTRACT(EPOCH FROM NOW()),
     UNIQUE(username),
     UNIQUE(email)
 );
@@ -14,4 +14,11 @@ RETURNS VOID
 LANGUAGE SQL
 AS $$
     INSERT INTO users VALUES (id, username, email, password);
+$$;
+
+CREATE OR REPLACE FUNCTION get_user_by_email(_email varchar(255))
+RETURNS TABLE (id varchar(64), username varchar(50), email varchar(255), password varchar(255), created_at integer)
+LANGUAGE SQL
+AS $$
+    SELECT * FROM users WHERE email = _email LIMIT 1;
 $$;
