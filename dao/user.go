6ba3ea85517ea4a8453f47c84ee4f9b9a7bc49d1
Semir Mahovkic <semir.mahovkic@ministryofprogramming.com>
2018-10-01 20:22:39 +0200

    Three main steps to register user account

--- dao/user.go
+++ dao/user.go
@@ -1,26 +1,46 @@
 package dao
 
-import "github.com/x64puzzle/spotted-proto/auth"
+import (
+	l "github.com/x64puzzle/spotted-common/log"
+	"github.com/x64puzzle/spotted-common/util"
+	pb "github.com/x64puzzle/spotted-proto/auth"
+)
 
 // User Data Access Object
 type User struct{}
 
 // Register user account
-func (u *User) Register(req *auth.RegisterRequest) (*auth.RegisterResponse, error) {
-	return nil, nil
+func (u *User) Register(req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
+	l.Log.Info("Register req: ", req)
+
+	uuid := util.UUID()
+
+	req.ID = uuid
+
+	// TODO: Insert into db
+
+	resp := &pb.RegisterResponse{}
+
+	return resp, nil
 }
 
 // Login user account
-func (u *User) Login(req *auth.LoginRequest) (*auth.LoginResponse, error) {
-	return nil, nil
+func (u *User) Login(req *pb.LoginRequest) (*pb.LoginResponse, error) {
+	l.Log.Info("Login req: ", req)
+
+	return &pb.LoginResponse{}, nil
 }
 
 // Logout user account
-func (u *User) Logout(req *auth.LogoutRequest) (*auth.LogoutResponse, error) {
-	return nil, nil
+func (u *User) Logout(req *pb.LogoutRequest) (*pb.LogoutResponse, error) {
+	l.Log.Info("Logout req: ", req)
+
+	return &pb.LogoutResponse{}, nil
 }
 
 // PasswordReset for user account
-func (u *User) PasswordReset(req *auth.PasswordResetRequest) (*auth.PasswordResetResponse, error) {
-	return nil, nil
+func (u *User) PasswordReset(req *pb.PasswordResetRequest) (*pb.PasswordResetResponse, error) {
+	l.Log.Info("PasswordReset req: ", req)
+
+	return &pb.PasswordResetResponse{}, nil
 }
--- service/server.go
+++ service/server.go
@@ -4,18 +4,41 @@ import (
 	"context"
 
 	"github.com/x64puzzle/spotted-auth/dao"
-	"github.com/x64puzzle/spotted-proto/auth"
+	"github.com/x64puzzle/spotted-common/util"
+	pb "github.com/x64puzzle/spotted-proto/auth"
 )
 
 // Server implements spotted-proto/auth.Service
 type Server struct{}
 
 // Register implements spotted-proto/auth.Service.Register
-func (s *Server) Register(ctx context.Context, req *auth.RegisterRequest) (*auth.RegisterResponse, error) {
+func (s *Server) Register(ctx context.Context, req *pb.RegisterRequest) (*pb.RegisterResponse, error) {
 	userDao := &dao.User{}
 
+	// Register user in db
 	resp, err := userDao.Register(req)
 	if err != nil {
+		return nil, err
+	}
+
+	// Generate jwt token for user
+	jwt := util.NewToken()
+
+	token, err := jwt.Generate(map[string]string{
+		"username": req.Username,
+		"email":    req.Email,
+		"id":       req.ID,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	resp.Token = token
+
+	// Create login session
+	session := &dao.Session{}
+
+	if err := session.Create(resp.Token); err != nil {
 		return nil, err
 	}
 
@@ -23,7 +46,7 @@ func (s *Server) Register(ctx context.Context, req *auth.RegisterRequest) (*auth
 }
 
 // Login implements spotted-proto/auth.Service.Login
-func (s *Server) Login(ctx context.Context, req *auth.LoginRequest) (*auth.LoginResponse, error) {
+func (s *Server) Login(ctx context.Context, req *pb.LoginRequest) (*pb.LoginResponse, error) {
 	userDao := &dao.User{}
 
 	resp, err := userDao.Login(req)
@@ -35,7 +58,7 @@ func (s *Server) Login(ctx context.Context, req *auth.LoginRequest) (*auth.Login
 }
 
 // Logout implements spotted-proto/auth.Service.Logout
-func (s *Server) Logout(ctx context.Context, req *auth.LogoutRequest) (*auth.LogoutResponse, error) {
+func (s *Server) Logout(ctx context.Context, req *pb.LogoutRequest) (*pb.LogoutResponse, error) {
 	userDao := &dao.User{}
 
 	resp, err := userDao.Logout(req)
@@ -47,7 +70,7 @@ func (s *Server) Logout(ctx context.Context, req *auth.LogoutRequest) (*auth.Log
 }
 
 // PasswordReset implements spotted-proto/auth.Service.PasswordReset
-func (s *Server) PasswordReset(ctx context.Context, req *auth.PasswordResetRequest) (*auth.PasswordResetResponse, error) {
+func (s *Server) PasswordReset(ctx context.Context, req *pb.PasswordResetRequest) (*pb.PasswordResetResponse, error) {
 	userDao := &dao.User{}
 
 	resp, err := userDao.PasswordReset(req)
